2990ed08edabdf5bf7141613c4b3d9a2f7287c40
Frank Melian <frankmelian@Franks-MacBook-Pro.local>
2017-01-02 11:44:40 -0500

    It work EUREKA this is the most hacky way but it does lololololololololol

--- js/script.js
+++ js/script.js
@@ -7,25 +7,19 @@ var farenheith = function(kelvinUnit){
 	return kelvinUnit - 459.67
 }
 
-var cityName = '';
+
 $.ajax({
 	url:'http://ip-api.com/json',
 	type:"GET",
 	dataType:"json",
 	crossDomain:'true',
 	success:function(data){
-     console.log(data);
-     cityName = data.city;
-     console.log(cityName);
-	},
-});
-
-$.ajax({
-url:'http://api.openweathermap.org/data/2.5/weather?q='+ cityName +' &appid=dee682908500fe0e34abbb0ffba910ec',
-type: "GET",
-dataType: 'jsonP',
-crossDomain:true,
-success:function(data){
+    $.ajax({
+    url:'http://api.openweathermap.org/data/2.5/weather?q='+data.zip+'&appid=dee682908500fe0e34abbb0ffba910ec',
+    type: "GET",
+    dataType: 'jsonP',
+    crossDomain:true,
+    success:function(data){
     console.log(data);
     $('#cityLocation').html(data.name);
     $('#humidity').html(data.main.humidity);
@@ -33,4 +27,10 @@ success:function(data){
     $('#tweet').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+ encodeURIComponent('"' + "weatherApp" + '"'));
   },
   cache:false,
+});    
+     
+
+	},
 });
+
+
